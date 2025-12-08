69d892fe42af31e8857634aa176cb184cce135cf
Adeyemi-Muaz-Ayomide <adeyemimuaz1@gmail.com>
2025-12-08 17:19:31 +0100

    Fixed Minor Issues

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -12,3 +12,8 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// export const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// );
